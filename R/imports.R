01f9444f06040311671c8f7f50dc5386afa4cbfc
Justin Landis <jtlandis@pop-os.localdomain>
2021-11-15 16:45:15 -0500

    need call2

--- R/imports.R
+++ R/imports.R
@@ -3,7 +3,7 @@
 #' @import shiny
 #' @import R6
 #' @importFrom sortable sortable_js
-#' @importFrom rlang `%||%` `!!!` abort warn as_function eval_tidy enquo
+#' @importFrom rlang `%||%` `!!!` abort warn as_function eval_tidy enquo call2
 #' @importFrom tidyselect eval_select
 #' @importFrom shinyjs useShinyjs show hide disable enable addClass
 #' @importFrom glue glue glue_collapse
