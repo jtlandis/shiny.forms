897655a5696ac4d185b7a80b1540875a0388e83d
Justin Landis <jtlandis@pop-os.localdomain>
2021-11-15 16:45:05 -0500

    refactoring

--- R/utilities.R
+++ R/utilities.R
@@ -83,3 +83,55 @@ row_bind <- function(..., check = TRUE) {
 
 
 }
+
+
+useShinyForms <- function() {
+  tags$head(
+    tags$style(HTML("
+                    .ShinyForm-break {
+                      line-height: 100%;
+                    }
+                    .ShinyForm-Preview-Container {
+                      padding: 15px;
+                    }
+                    .ShinyForm-Container {
+                      padding: 10px;
+                    }
+                    .ShinyForm-Menu {
+                      padding: 5px;
+                    }
+                    /*.ShinyForm-Element:hover {
+                      background-color: #7682FF;
+                      opacity: .5;
+                    }*/
+                    .ShinyForm-Element-selected {
+                      border: 2px dotted grey;
+                    }
+                    .ShinyForm-Column {
+                      border: .5px solid grey;
+                      padding: 25px;
+                      border-radius: 15px;
+                    }
+                    .ShinyForm-selected,
+                    .ShinyForm-Element.ShinyForm-selected,
+                    .ShinyForm-Column.ShinyForm-selected {
+                      border: 5px solid red;
+                      opacity: .5;
+                    }
+                    /*.ShinyForm-Column-selected:hover {
+                      background-color: #FF7676;
+                      opacity: .5;
+                    }*/
+                    "))
+  )
+}
+
+useShinyFormsLite <- function() {
+  tags$head(
+    tags$style(HTML("
+                    .ShinyForm-Container {
+                      padding: 10px;
+                    }
+                    "))
+  )
+}
--- example-builder.R
+++ example-builder.R
@@ -5,44 +5,6 @@ library(shiny)
 
 test <- ShinyFormBuilder$new("test_id")
 shinyApp(ui = fluidPage(
-  tags$head(
-    tags$style(HTML("
-                    .ShinyForm-break {
-                      line-height: 100%;
-                    }
-                    .ShinyForm-Preview-Container {
-                      padding: 15px;
-                    }
-                    .ShinyForm-Container {
-                      padding: 10px;
-                    }
-                    .ShinyForm-Menu {
-                      padding: 5px;
-                    }
-                    /*.ShinyForm-Element:hover {
-                      background-color: #7682FF;
-                      opacity: .5;
-                    }*/
-                    .ShinyForm-Element-selected {
-                      border: 2px dotted grey;
-                    }
-                    .ShinyForm-Column {
-                      border: .5px solid grey;
-                      padding: 25px;
-                      border-radius: 15px;
-                    }
-                    .ShinyForm-selected,
-                    .ShinyForm-Element.ShinyForm-selected,
-                    .ShinyForm-Column.ShinyForm-selected {
-                      border: 5px solid red;
-                      opacity: .5;
-                    }
-                    /*.ShinyForm-Column-selected:hover {
-                      background-color: #FF7676;
-                      opacity: .5;
-                    }*/
-                    "))
-  ),
   test$ui(),
   actionButton(".browse", "Browse")), function(input, output, session){
 
